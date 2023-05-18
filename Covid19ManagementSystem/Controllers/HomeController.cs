814238ae31742a5b349820460bea880da7fdff60
אודיה סעדון <osadoun@g.jct.ac.il>
2023-05-19 02:02:27 +0300

    add homecontroller and home view

---
+++ Covid19ManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Covid19ManagementSystem.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddContent()
+        {
+            return View();
+        }
+
+        public IActionResult HmoMembers()
+        {
+            return View();
+        }
+
+        public IActionResult Statistics()
+        {
+            return View();
+        }
+    }
+}
---
+++ Covid19ManagementSystem/DatabaseInitializer.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.IO;
+
+namespace Covid19ManagementSystem
+{
+
+    public static class DatabaseInitializer
+    {
+        public static void InitializeDatabase(IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    MySqlCommand command = connection.CreateCommand();
+
+                    // Create the database
+                    command.CommandText = "CREATE DATABASE IF NOT EXISTS coronadatabase;";
+                    command.ExecuteNonQuery();
+
+                    // Switch to the database
+                    command.CommandText = "USE coronadatabase;";
+                    command.ExecuteNonQuery();
+
+                    // Read and execute the schema creation SQL queries from a file
+                    string sqlFilePath = Path.Combine(Directory.GetCurrentDirectory(), "../SQLFiles/craeteTables.sql");
+                    string sqlQueries = File.ReadAllText(sqlFilePath);
+
+                    command.CommandText = sqlQueries;
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+
+}
